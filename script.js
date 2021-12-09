ccb48d67f2a369c02d002b5358c24c7721d35af8
kainfelyne <rainingblues13@gmail.com>
2021-12-08 19:46:12 -0600

    prueba de operadores

---
+++ script.js
@@ -0,0 +1,36 @@
+//###MATEMATICOS###
+
+let a = 5;
+let b = 2;
+
+let texto = "El resultado de ";
+
+let suma = "a + b ";
+let resta = "a - b ";
+let multi = "a x b ";
+let divi = "a / b ";
+let modu = "a % b ";
+
+console.log( texto + suma + "es: ", a + b );
+console.log( texto + resta + "es: ", a - b );
+console.log( texto + multi + "es: ", a * b );
+console.log( texto + divi + "es: ", a / b );
+console.log( texto + modu + "es: ", a % b );
+
+
+
+//###INCREMENTO/DECREMENTO###
+
+let c = 5;
+
+console.log(c);
+console.log(c++); //Primero devuelve la variable y luego se incrementa
+console.log(c);
+console.log(++c); //Primero incrementa y luego devuelve la variable
+
+let d = 9;
+
+console.log(d);
+console.log(d--);
+console.log(d);
+console.log(--d);
