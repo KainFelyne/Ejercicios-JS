40442b3d8e756049e003d86d8794d41ab12c850d
kainfelyne <rainingblues13@gmail.com>
2021-12-13 23:45:00 -0600

    cambios strings

--- Strings/script.js
+++ Strings/script.js
@@ -21,3 +21,10 @@ console.log(cadenaMayus);
 
 //*toLowerCase() -> La cadena se muestra en minusculas
 console.log(cadenaMayus.toLowerCase());
+
+//+indexOf() -> Devuelve la posición en la que se encuentra el parametro indicado. Si no lo encuentra devuelve -1
+console.log(cadena.indexOf('m'));
+
+//*replace(valor a buscar, valor nuevo) -> Remplaza el valor de la cadena que indiquemos y pone el valor nuevo
+console.log(cadena.replace('m', 'M'));
+console.log(cadena.replace('Hola', 'Adios'));
