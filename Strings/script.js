94b9855fef79a1912a7e902cf5be85e7c9100948
kainfelyne <rainingblues13@gmail.com>
2021-12-13 22:19:42 -0600

    strings

---
+++ Strings/script.js
@@ -0,0 +1,23 @@
+//###STRNGS###
+
+let cadena = "Hola mundo";
+
+//PROPIEDADES
+
+//length -> Devuelve la longitud de la cadena
+
+console.log(cadena.length);
+
+//METODOS
+//Los metodos devuelven cadenas nuevas
+
+//*toUpperCase() -> La cadena se muestra en mayúsculas
+console.log(cadena.toUpperCase());
+console.log(cadena);
+
+let cadenaMayus = cadena.toUpperCase();
+
+console.log(cadenaMayus);
+
+//*toLowerCase() -> La cadena se muestra en minusculas
+console.log(cadenaMayus.toLowerCase());
