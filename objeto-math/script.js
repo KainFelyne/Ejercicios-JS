69d7fd0213db5d9392ef913d7b56ed35ecbe218f
kainfelyne <rainingblues13@gmail.com>
2021-12-16 17:12:15 -0600

    Math

---
+++ objeto-math/script.js
@@ -0,0 +1,25 @@
+//###OBJETO MATH###
+
+
+//**Metodos
+
+console.log(Math.E);
+console.log(Math.PI);
+
+let num = 5.9;
+let num2 = 2;
+//*Math.abs() -> Devuelve el valor absoluto.
+console.log("**Math.abs()**")
+console.log(Math.abs(num));
+
+//*Math.ceil() -> Devuelve el entero mas grande
+console.log("**Math.ceil()**")
+console.log(Math.ceil(num));
+
+//*Math.floor() -> Devuelve el entero mas pequeño
+console.log("**Math.floor()**")
+console.log(Math.floor(num));
+
+//*Math.pow() -> Devuelve la potencia de x elevado a y
+console.log("**Math.pow()**")
+console.log(Math.pow(2, 3));
