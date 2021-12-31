667126d69db6e2a542eab8b1da50f97d2904fd52
kainfelyne <rainingblues13@gmail.com>
2021-12-30 23:15:45 -0600

    mas arrays

---
+++ arrays-2/script.js
@@ -0,0 +1,34 @@
+let numbers = [1,2,3,4,5];
+
+let number = 3;
+// Array.isArray
+console.log("**Array.isArry**");
+console.log(Array.isArray(numbers));
+console.log(Array.isArray(number));
+
+//Eliminar elemento
+// .shift() -> Elimina el primer elemento del array
+// . pop() -> Elimina el ultimo elemento del array
+
+console.log("**.shift()**")
+console.log(numbers);
+numbers.shift();
+console.log(numbers);
+console.log("**.pop()**");
+numbers.pop();
+console.log(numbers);
+
+//Añadir elemento
+// .unshift() -> Añade uno o mas elemento al inicio
+// .push() -> Añade uno o mas elementos al final
+
+let numbers2 = [6, 7, 8, 9];
+
+console.log("**.push()**");
+console.log(numbers2);
+numbers2.push(10);
+
+console.log("**.unshift()**");
+console.log(numbers2);
+numbers2.unshift(5);
+console.log(numbers2);
