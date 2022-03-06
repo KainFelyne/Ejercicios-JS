f4ab5ea4eef43bf471466d6f220a92bcf12f2360
kainfelyne <rainingblues13@gmail.com>
2022-03-06 08:18:15 -0600

    funciones

---
+++ funciones/script.js
@@ -0,0 +1,34 @@
+/*
+function saludar (){
+    console.log(`Hola`);
+}
+
+saludar()
+saludar()
+saludar()
+*/
+
+/*
+const saludar = () => console.log(`Hola`)
+const saludarUsuario = (user) => console.log(`Hola ${user}`)
+
+saludarUsuario(`Brownie`)
+*/
+
+/*
+const suma = (num1, num2) => {
+   if(num1 == 2){
+    return num1 + num2
+   }
+
+   return num1
+
+}
+
+console.log(suma(2, 3))
+*/
+
+const suma = (num1, num2) => num1 + num2
+
+let resultado = suma (3,6)
+console.log(resultado)
