fa0e99b6ab9eb6ad639b45c6cf08adf50668e0aa
kainfelyne <rainingblues13@gmail.com>
2022-03-07 19:58:56 -0600

    clases

---
+++ programacion-orientada-a-objetos/script.js
@@ -0,0 +1,21 @@
+class Persona {
+    
+    constructor(nombre, apellido, edad){
+        this.nombre = nombre
+        this.apellido = apellido
+        this.edad = edad
+
+        this.datos = `Me llamo ${this.nombre} ${this.apellido} y tengo ${this.edad} años.`
+    }
+
+    saludar(){
+        return `"Hola me llamo ${this.nombre} y tengo ${this.edad} años".`
+    }
+}
+
+const juan = new Persona('Juan', 'García', 25)
+const marta = new Persona('Marta', 'García', 35)
+
+console.log(juan)
+console.log("*****Saludo*****")
+console.log(juan.saludar())
