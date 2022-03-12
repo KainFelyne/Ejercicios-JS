b461a440a94e83c5383e279fb350c7f6388358a7
kainfelyne <rainingblues13@gmail.com>
2022-03-12 12:56:21 -0600

    spread

--- spread-operator/script.js
+++ spread-operator/script.js
@@ -15,10 +15,43 @@ addNumber(1, 2, 3)
 
 //Añadir un array a otro array
 
+/*
 let users = ['javier', 'david', 'rosa', 'juan', 'mercedes']
 
 let newUsers = ['marta', 'jaime', 'laura']
 
-users.push(...newUsers)
+users.push(...newUsers) // se puede usar unshift para agregarlos al principio
 
 console.log(users)
+*/
+
+
+
+//Copiar array
+ 
+/*
+let arr1 = ['1', '2', '3', '4']
+let arr2 = [...arr1]
+
+console.log(arr2)
+*/
+
+//Concatenar arrays
+
+/*
+let arr1 = ['1', '2', '3', '4', '5']
+let arr2 = ['6', '7', '8']
+
+//let arrConcat = arr1.concat(arr2)
+let arrConcat = [...arr1, ...arr2]
+
+console.log(arrConcat)
+*/
+
+// Enviar un número indefinido de argumento a una función (parametro REST)
+
+const restParms = (...numbers) => {
+    console.log(numbers)
+}
+
+restParms()
