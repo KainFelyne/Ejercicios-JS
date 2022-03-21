3d61f6a1284a0929de6cc93a8b95282d2107e242
kainfelyne <rainingblues13@gmail.com>
2022-03-20 21:10:31 -0600

    objeto evento

---
+++ objeto-evento/script.js
@@ -0,0 +1,13 @@
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+
+input.addEventListener('keyup', (e) =>{
+    console.log(e);
+})
+
+/*
+window.addEventListener('click', (e) => {
+    console.log(e)
+})
+*/
+
