5df5862cc739184b7206615423bc3c8a16d153f9
kainfelyne <rainingblues13@gmail.com>
2022-03-22 17:02:58 -0600

    eventos de objeto

--- objeto-evento/script.js
+++ objeto-evento/script.js
@@ -1,13 +1,50 @@
 const form = document.getElementById('form')
 const input = document.getElementById('input')
+const button = document.getElementById('button')
+const link = document.getElementById('link')
 
-input.addEventListener('keyup', (e) =>{
-    console.log(e);
-})
+//input.addEventListener('keyup', (e) =>{
+    //console.log(e);
+    //console.log(e.key);
+    //console.log(e.path[0]);
+//})
 
 /*
-window.addEventListener('click', (e) => {
+window.addEventListener('click', (e) => {  ->  objeto global window
     console.log(e)
 })
 */
 
+/*
+button.addEventListener('click', (e) =>{
+    console.log(e.target)
+})
+*/
+
+/* GALLERY****
+const gallery = document.getElementById ('gallery')
+
+gallery.addEventListener('click', (e) =>{
+    console.log(e.target.textContent)
+    e.target.classList.add('red')
+})
+*/
+
+/*
+*/
+
+
+link.addEventListener('click', (e) => {
+    e.preventDefault()
+    button.click()
+    console.log('Dirigiendo a google')
+})
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    console.log('El formulario se ha enviado')
+})
+
+button.addEventListener('click', () =>{
+    input.value = 'Has hecho click'
+})
