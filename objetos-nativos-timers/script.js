afcfe3d0e49f1b464d0d196018c4b3da1bb9c88f
kainfelyne <rainingblues13@gmail.com>
2022-03-27 20:36:20 -0600

    objetos nativos

---
+++ objetos-nativos-timers/script.js
@@ -0,0 +1,84 @@
+/*
+    Objeto window - Es el objeto global, de el descienden todos los objetos
+        alert()
+        prompt()
+        confirm()
+*/
+
+//alert('Hola Ely')
+
+/*
+addEventListener('click', (e) =>{
+    console.log(e)
+})
+*/
+
+/*
+if (confirm('¿Acepta?')){
+    console.log('Aceptado')
+}
+else{
+    console.log('No aceptado')
+}
+*/
+
+
+
+
+/*
+    Objeto console - Es el objeto que contiene la consola del navegador
+        console.log()
+        console.dir()
+        console.error()
+        console.table()
+*/
+
+
+const button = document.getElementById('button')
+//console.log(button)
+//console.dir(button)
+//console.error('Error')
+
+/*
+const persona = {
+    name: 'Ely',
+    age: 32,
+    email: 'ely@ely.com'
+}
+
+console.table(persona)
+
+const numbers = [1, 2, 3, 4]
+console.table(numbers)
+*/
+
+/*
+    Objeto location - Es el objeto que tiene la barra de dierecciones
+        location.href
+        location.protocol
+        location.host
+        location.pathname
+        location.hash
+        location.reload()
+*/
+
+
+/*
+console.log(location.href)
+console.log(location.protocol)
+console.log(location.host)
+console.log(location.pathname)
+console.log(location.hash)
+//location.reload())
+*/
+
+//location.href = 'https://google.com' <- Redirigir a otra pagina
+
+/*
+    Objeto history
+    back()
+    forward()
+    go(n | -n)
+    length
+*/
+
